9f2c3818f7a395fb8195beb4727533bf24111343
Duncan Chong <duncan.chong@proton.me>
2023-06-09 10:18:36 -0400

    Working prototype

--- askaway/__main__.py
+++ askaway/__main__.py
@@ -1,5 +1,27 @@
+import vercel_ai
+import json
+from tabulate import tabulate
+
 def main():
-    print("Hello world!")
+    client = vercel_ai.Client()
+    models = ''.join(json.dumps(client.model_ids))[1:-1].replace('\"', '').split(', ')
+    models_table = []
+    for i, model in enumerate(models):
+        models_table.append(
+            [i, model]
+        )
+    print(
+        tabulate(
+            models_table,
+            headers=["#", "Model"],
+            tablefmt="fancy_grid",
+        )
+    )
+    model_choice = int(input("Choose a model #: "))
+    prompt = input("Enter a prompt: ")
+    for chunk in client.generate(models[model_choice], prompt):
+        print(chunk, end="", flush=True)
+    print()
 
 
 if __name__ == "__main__":
